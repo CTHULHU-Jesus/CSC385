3a13ff307b455727b597868cb89c029bc10719a9
Matthew Bartlett <endercreeper92@gmail.com>
2021-03-03 16:54:31 -0500

    Added more to Checkers

--- TextTermGames/src/Checkers.hs
+++ TextTermGames/src/Checkers.hs
@@ -27,10 +27,11 @@ data Player
 
 -- (isKing,Player)
 data Checker = NonKing Player
-            | King    Player
-            deriving (Eq)
+             | King     Player
+             deriving (Eq)
 
 type Board = M.Matrix (Maybe Checker)
+
 
 data State 
         = AISelectorMenu (BWList.List String (Maybe Player))
@@ -39,64 +40,49 @@ data State
             { board :: Board
             , currPlayer :: Player
             , jumpingPiece :: Maybe (Int,Int)
+            , move :: Maybe (Int,Int)
             , currCursorPos :: (Int,Int)
             , aiOponnent :: Maybe Player
             }
 -- Constants
+initState :: State
+initState = 
+    let
+        selectList = Vec.fromList [Nothing, Just White, Just Black]
+    in
+        AISelectorMenu $ BWList.list "Checkers-AI-List" selectList 2 
 
-initState = State 
-    { board = M.matrix 8 8 (\_ -> Nothing)
+
+
+initBoard :: State
+initBoard = State 
+    { board = M.matrix 8 8 
+        (\(y,x) -> if (x+y) `mod` 2 == 1 then
+                       case y of
+                            y' | y' <= 3 -> Just (NonKing Black)
+                            y' | y' >= 6 -> Just (NonKing White)
+                            _ -> Nothing
+                   else
+                       Nothing
+        )
     , currPlayer = White
     , jumpingPiece = Nothing
+    , move = Nothing
     , currCursorPos = (0,0)
     , aiOponnent = Nothing }
     
 -- Functions
 
-upBoard  :: Board -> State -> State
-upBoard board (State 
-            { board = _
-            , currPlayer = currPlayer
-            , jumpingPiece = jumpingPiece
-            , currCursorPos = currCursorPos
-            , aiOponnent = aiOponnent
-            }) =
-    State { board = board
-          , currPlayer = currPlayer
-          , jumpingPiece = jumpingPiece
-          , currCursorPos = currCursorPos
-          , aiOponnent = aiOponnent }
-upBoard _ x = x
+other :: Player -> Player
+other Black = White
+other White = Black
 
-upPlayer :: Player -> State -> State
-upPlayer currPlayer (State 
-            { board = board
-            , currPlayer = _
-            , jumpingPiece = jumpingPiece
-            , currCursorPos = currCursorPos
-            , aiOponnent = aiOponnent
-            }) =
-    State { board = board
-          , currPlayer = currPlayer
-          , jumpingPiece = jumpingPiece
-          , currCursorPos = currCursorPos
-          , aiOponnent = aiOponnent }
-upPlayer _ x = x
+upOne :: (Int,Int) -> (Int,Int)
+upOne (y,x) = (y+1,x+1)
 
-upJump :: Maybe (Int,Int) -> State -> State
-upJump jumpingPiece (State 
-            { board = board
-            , currPlayer = currPlayer
-            , jumpingPiece = _
-            , currCursorPos = currCursorPos
-            , aiOponnent = aiOponnent
-            }) =
-    State { board = board
-          , currPlayer = currPlayer
-          , jumpingPiece = jumpingPiece
-          , currCursorPos = currCursorPos
-          , aiOponnent = aiOponnent }
-upJump _ x = x
+colorOf :: Checker -> Player
+colorOf (NonKing x) = x
+colorOf (King    x) = x
 
 
 upCursor :: (Int,Int) -> State -> State
@@ -104,12 +90,14 @@ upCursor currCursorPos (State
             { board = board
             , currPlayer = currPlayer
             , jumpingPiece = jumpingPiece
+            , move = move
             , currCursorPos = _
             , aiOponnent = aiOponnent
             }) =
     State { board = board
           , currPlayer = currPlayer
           , jumpingPiece = jumpingPiece
+          , move = move
           , currCursorPos = currCursorPos
           , aiOponnent = aiOponnent }
 upCursor _ x = x
@@ -120,50 +108,215 @@ upAI aiOponnent (State
             { board = board
             , currPlayer = currPlayer
             , jumpingPiece = jumpingPiece
+            , move = move
             , currCursorPos = currCursorPos
             , aiOponnent = _
             }) =
     State { board = board
           , currPlayer = currPlayer
           , jumpingPiece = jumpingPiece
+          , move = move
           , currCursorPos = currCursorPos
           , aiOponnent = aiOponnent }
 upAI _ x = x
 
 
 draw :: State -> [Widget String] 
-draw state@(State 
+draw state@State 
             { board = board
             , currPlayer = currPlayer
             , jumpingPiece = jumpingPiece
+            , move = move
             , currCursorPos = currCursorPos
             , aiOponnent = aiOponnent
-            }) = 
+            } = 
     let
         rend :: Board -> String
-        rend board = ( init
+        rend m = ( init
             . foldl (\a b -> a++"\n"++b) ""
-            . map (foldl ++ "")
+            . map (foldl (++) "")
             . M.toLists
             . M.mapPos (\(y,x) piece ->
-            case piece of
-                Just x  -> show x
-                Nothing -> if (y+x) `mod` 2 then
-                               " "
-                           else
-                               "░"
-            $ board))
+                case piece of
+                    Just x  -> show x
+                    Nothing -> if 1==(y+x) `mod` 2 then
+                                   " "
+                               else
+                                   "░")
+            $ m)
 
         label :: String
         label = "Go " ++ show currPlayer
     in
         [ center
-        . showCursor "use" (Location currCursorPos)
-        . withBorderStyle unicode hBorder
-        . str
+        . showCursor "use" 
+            ( Location 
+            . (\(y,x) -> (x+1,y+2))
+            $ currCursorPos)
+        -- . withBorderStyle unicode hBorder
         . borderWithLabel (str label)
+        . str
         . rend
         $ board]
+
+handelEvents :: State -> BrickEvent e () -> EventM e (Next State)
+handelEvents state@(State 
+            { board = board
+            , currPlayer = currPlayer
+            , jumpingPiece = jumpingPiece
+            , move = move
+            , currCursorPos = (cursorY,cursorX)
+            , aiOponnent = aiOponnent
+            }) e =
+    let
+        ups    = [V.KChar 'k', V.KUp]
+        downs  = [V.KChar 'j', V.KDown]
+        rights = [V.KChar 'l', V.KRight]
+        lefts  = [V.KChar 'h', V.KLeft]
+    in
+    case e of
+        VtyEvent (V.EvKey key _)-> case key of
+            x | x `elem` ups -> 
+                Brick.Main.continue
+                $ upCursor ((cursorY-1) `mod` 8,cursorX) state
+
+            x | x `elem` downs -> 
+                Brick.Main.continue
+                $ upCursor ((cursorY+1) `mod` 8,cursorX) state
+
+            x | x `elem` rights -> 
+                Brick.Main.continue
+                $ upCursor (cursorY,(cursorX+1) `mod` 8) state
+
+            x | x `elem` lefts -> 
+                Brick.Main.continue
+                $ upCursor (cursorY,(cursorX-1) `mod` 8) state
+
+            V.KEnter -> 
+                Brick.Main.continue
+                . checkWinner
+                . runAI
+                . checkWinner
+                . update
+                $ state
+
+            _ -> Brick.Main.continue state
+
+        _ -> Brick.Main.continue state
+
+handelEvents x e = Brick.Main.continue x
+
+-- Someone can win the game by capturing all of there opponents pieces
+-- or by making so there opponent can't move.
+checkWinner :: State -> State
+checkWinner state@State 
+                { board = board
+                , currPlayer = _
+                , jumpingPiece = _
+                , move = _
+                , currCursorPos = _
+                , aiOponnent = _
+                } = 
+    let
+        piecesActive :: [ Checker ]
+        piecesActive = catMaybes
+                     . M.toList
+                     $ board
+
+        blackActive :: Bool
+        blackActive = any (==Black)
+                    . map (\x -> case x of
+                             NonKing c -> c
+                             King    c -> c)
+                    $ piecesActive
+
+        whiteActive :: Bool
+        whiteActive = any (==White)
+                    . map (\x -> case x of
+                             NonKing c -> c
+                             King    c -> c)
+                    $ piecesActive
+    in
+        if not blackActive then
+            Winner $ Just White
+        else if not whiteActive then
+            Winner $ Just Black
+        else if [] == validMoves board Black then
+            Winner $ Just White
+        else if [] == validMoves board White then
+            Winner $ Just Black
+        else
+            state
+
+checkWinner x = x
+
+update :: State -> State
+update state@State 
+            { board = board
+            , currPlayer = currPlayer
+            , jumpingPiece = jumpingPiece
+            , move = move
+            , currCursorPos = currCursorPos@(cursorY,cursorX)
+            , aiOponnent = aiOponnent
+            } =
+    case move of
+        Nothing      -> 
+            -- if color of piece == currPlayer then can move
+            case board M.! (upOne currCursorPos) of
+                Nothing -> state
+                Just x  -> if colorOf x == currPlayer then
+                               state {move = Just currCursorPos}
+                           else
+                               state
+        Just (y1,x1) -> 
+            let
+                move = ((y1,x1),currCursorPos)
+            in
+                if move `elem` (validMoves board currPlayer) then
+                    let
+                        board' = applyMove move board
+                    in
+                        if canJump board' currCursorPos then
+                            state { board = board'
+                                        , move = Just currCursorPos } 
+                        else
+                            state { board = board'
+                                        , move = Nothing
+                                        , currPlayer = other currPlayer}
+                else
+                    state 
+update x = x
+
+canJump :: Board -> (Int,Int) -> Bool
+canJump board pos =
+    case board M.! (upOne pos) of
+        Nothing -> False
+        Just (NonKing _) -> False
+        Just (King    _) -> False-- @TODO
+
+validMoves :: Board -> Player -> [((Int,Int),(Int,Int))]
+validMoves board player =
+    foldl (++) []
+    . M.toList
+    . M.mapPos 
+        (\(y,x) piece -> 
+            case piece of
+                Nothing -> []
+                Just piece ->
+                    if colorOf piece == player then
+                        [] -- @TODO makable moves
+                    else
+                        [])
+    $ board
+
+
+applyMove :: ((Int,Int),(Int,Int)) -> Board -> Board
+applyMove move board = -- @TODO
+    board
+    
+
+runAI :: State -> State
+runAI = id
 
 -- Instances
 instance Show Checker where
